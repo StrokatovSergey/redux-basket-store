c87789b6ef57e5e7b74f6a1ef2f31831cf2d5d4f
StrokatovSergey <s.strokatov@mail.ru>
2020-11-04 23:29:55 +0300

    master | frontend | made updating cart by 'add ti cart' in redux and ShoppingCartList

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,21 +3,18 @@ const initialState = {
   books: [],
   loading: true,
   cartItems: [
-    {
-      id: 1,
-      name: 'Release It!',
-      count: 3,
-      total: 130
-    },
-    {
-      id: 2,
-      name: 'Production-Ready Microservices\n',
-      count: 3,
-      total: 130
-    }
+
   ],
   orderTotal: 600
 };
+
+const updateCarItems = (cartItems, item, index) => {
+  if (index === -1) {
+    return  [ ...cartItems, item ]
+  } else {
+    return [ ...cartItems.slice(0, index), item, ...cartItems.slice(index + 1)  ]
+  }
+}
 
 const reducer = (state = initialState, action) => {
 
@@ -45,19 +42,40 @@ const reducer = (state = initialState, action) => {
       }
     case 'BOOK_ADDED_TO_CART':
       const bookId = action.payload
-      const book = state.cartItems.find((book) => book.id === bookId)
-      const newItem = {
-        id: book.id,
-        name: book.name,
-        count: 1,
-        total: book.price
+      const book = state.books.find((book) => book.id === bookId)
+      const itemIndex = state.cartItems.findIndex(book => book.id === bookId)
+      const item = state.cartItems[itemIndex]
+      let newItem;
+      if (item) {
+          newItem = {
+            ...item,
+            count: ++item.count
+          }
+      } else {
+        newItem = {
+          id: book.id,
+          title: book.title,
+          count: 1,
+          total: book.price
+        }
       }
-      return {
-        ...state,
-        cartItems: [
-          ...state.cartItems,
-            newItem
-        ]
+      if (itemIndex < 0) {
+          return {
+            ...state,
+            cartItems: [
+              ...state.cartItems,
+              newItem
+            ]
+          }
+      } else {
+        return {
+          ...state,
+          cartItems: [
+            ...state.cartItems.slice(0, itemIndex),
+            newItem,
+            ...state.cartItems.slice(itemIndex + 1)
+          ]
+        }
       }
 
     default:
